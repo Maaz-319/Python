c62c43970d75f8bbb3a825090a733223ce594ce0
Maaz Bin Asif <maazbinaasif123@outlook.com>
2025-06-30 14:19:40 +0500

    Update main.py

--- Quiz/main.py
+++ Quiz/main.py
@@ -1,27 +1,22 @@
-def game():
-    quiz = {
+print("WELCOME TO QUIZ GAME")
+print("="*20)
+input("ENTER TO START")
+print("="*20)
+quiz = {
         "Which keyword is used to define a function in Python?": "def",
         "What is extension of python file?": ".py",
         "Which keyword is used to check a condition?": "if",
         "Set can store Duplicate Values? T/F": "F",
         "This is a very nice course. T/F": "T"
     }
+score = 0
     
-    score = 0
-    
-    for question, answer in quiz.items():
+for question, answer in quiz.items():
         print(f"\nQ: {question}")
         user_input = input("Your answer: ").lower()
-        if user_input == answer:
+    if user_input == answer:
             print("Correct!")
             score += 1
-        else:
-            print(f"Wrong! Correct answer: {answer}")
-    
-    print(f"\nYour final score: {score} out of {len(quiz)}")
-
-print("WELCOME TO QUIZ GAME")
-print("="*20)
-input("ENTER TO START")
-print("="*20)
-game()
+    else:
+        print(f"Wrong! Correct answer: {answer}")
+print(f"\nYour final score: {score} out of {len(quiz)}")
